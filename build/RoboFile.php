1a0107498e95dc95a8e4995f84119f7f767ded39
pixelbrackets <mail@pixelbrackets.de>
2017-12-14 17:51:03 +0100

    [BUGFIX] Sync: Avoid empty exclude argument
    If the fallback is a string, then Robo passes
    an empty »exclude« argument to Rsync. Avoid this
    by using an empty array as fallback instead.

--- build/RoboFile.php
+++ build/RoboFile.php
@@ -51,7 +51,7 @@ class RoboFile extends \Robo\Tasks
         foreach ($syncPaths as $syncPath) {
             $rsync = $this->taskRsync()
                 ->rawArg($stageProperties['rsync']['options'])
-                ->exclude($syncPath['exclude'] ?? '')
+                ->exclude($syncPath['exclude'] ?? [])
                 ->fromPath($this->getBuildProperty('repositoryPath') . $syncPath['source'])
                 ->toUser($stageProperties['user'])
                 ->toHost($stageProperties['host'])
