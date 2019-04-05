47872026ca6ef1d7ddbba922ba94c60927f8dc87
pixelbrackets <mail@pixelbrackets.de>
2019-04-05 14:39:41 +0200

    [FEATURE][!!] Assets: Change NPM install command
    Use »ci« as NPM installation command instead of »install« from now on.
    This command will respect the »package-json.lock« which causes
    everybody involved to have the same npm packages.
    Drawback: The command will remove the entire »node_modules« folder
    on every execution.
    
    Breaking Change (the install command will behave differently now
    and packages are cached in »~/.npm« and not »node_modules« anymore).

--- build/RoboFile.php
+++ build/RoboFile.php
@@ -194,7 +194,7 @@ class RoboFile extends \Robo\Tasks
         $gruntDirectory = $this->getBuildProperty('repository-path') . $gruntDirectory;
 
         $this->say('Install/Update Node Packages');
-        $this->taskExec('npm --silent --progress=false install')
+        $this->taskExec('npm --silent --progress=false ci')
             ->dir($gruntDirectory)
             ->run();
 
